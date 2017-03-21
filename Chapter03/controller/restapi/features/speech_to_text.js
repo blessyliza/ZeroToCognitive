0fc2b63bbc69dadbb1d33042f8b6fbaaf8868795
Blessy George <blessyliza@au1.ibm.com>
2017-03-21 17:43:10 +1100

    logic added for text to speech and speech to text

--- Chapter03/controller/restapi/features/speech_to_text.js
+++ Chapter03/controller/restapi/features/speech_to_text.js
@@ -4,5 +4,30 @@ var vcapServices = require('vcap_services');
 var config = require('../../env.json');
 
 exports.token = function(req, res) {
+    var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
 
+    var sttAuthService = watson.authorization(sttConfig);
+
+    sttAuthService.getToken({
+        url: sttConfig.url
+    }, function(err, token) {
+        if (err) {
+            console.log('Error retrieving token: ', err);
+            res.status(500).send('Error retrieving token');
+            return;
+        }
+        res.send(token);
+    });
+}
+exports.tts_synthesize = function(req, res) {
+  console.log("tts_synthesize entered");
+    var ttsConfig = watson.text_to_speech(config.text_to_speech);
+    var transcript = ttsConfig.synthesize(req.query);
+    transcript.on('response', function(response) {
+      if (req.query.download) {
+        response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+      }
+    });
+    transcript.on('error', function(error) { console.log("error encountered: "+error); next(error); });
+    transcript.pipe(res);
 }
